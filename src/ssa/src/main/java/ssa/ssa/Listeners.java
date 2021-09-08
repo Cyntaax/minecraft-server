a8f537ce8faddc389b9fd06866f95df78cd0ffea
Cyntaax C <cyntaax@gmail.com>
2021-09-08 06:40:30 -0400

    chore: remove event handler

--- src/ssa/src/main/java/ssa/ssa/Listeners.java
+++ src/ssa/src/main/java/ssa/ssa/Listeners.java
@@ -22,23 +22,12 @@ public class Listeners implements Listener {
         loginEvent.setKickMessage("Kicking for no reason at all");
     }
 
-    @EventHandler
-    public void onDestroy(BlockBreakEvent breakEvent) {
-        System.out.println("Evt catch");
-        Block block = breakEvent.getBlock();
-        breakEvent.getPlayer().chat("No breaking blocks!");
-        World world = Bukkit.getServer().getWorld("world");
-        assert world != null;
-        world.setTime(0);
-        world.setStorm(false);
-        breakEvent.setCancelled(true);
-    }
 
     @EventHandler
     public void onMove(PlayerMoveEvent moveEvent) {
         Location loc = moveEvent.getPlayer().getLocation().clone().subtract(0,1,0);
     }
-    
+
 
     @EventHandler
     public void onStart(PluginEnableEvent enableEvent) {
